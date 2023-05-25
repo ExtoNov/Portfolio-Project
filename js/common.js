f05257095e398f934307dabfed2ba3238351d5bf
ExtoNov <otaku0722@gmail.com>
2023-05-25 13:06:14 +0900

    edited
    modal bingding, resource upload

--- js/common.js
+++ js/common.js
@@ -1,8 +1,8 @@
-let scrollThumb = document.querySelector('.scroll-thumb');
-let scrollTrack = document.querySelector('#scroll-track');
 let scrollHeight;
 let sclTopbtn = document.querySelector('.scroll-top');
 let sectionArray;
+let scrollTrack = this.document.querySelector('.scroll-track');
+let scrollThumb = this.document.querySelector('.scroll-thumb');
 //DOM이 완전히 로드되었을때
 window.addEventListener('DOMContentLoaded', function(){
     scrollHeight = this.document.body.scrollHeight;
@@ -42,13 +42,13 @@ window.addEventListener('DOMContentLoaded', function(){
 });
 //scroll event
 let delay;
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function(e){
     let currentScrollY = window.scrollY;
     let docHeight = document.body.offsetHeight;
     let winHeight = window.innerHeight;
     let scrollPercent = currentScrollY / (docHeight - winHeight);
-    let scrollPercentRounded = Math.round(scrollPercent * 100);
-    scrollThumb.style.top = `${scrollPercentRounded}%`;
+    scrollPercent = scrollPercent * 100;
+    scrollThumb.style.top = `${scrollPercent}%`;
 
     if(currentScrollY > 600) {
         sclTopbtn.style.display = 'flex';
@@ -57,12 +57,12 @@ window.addEventListener('scroll', function(){
     }
 
     scrollTrack.style.opacity = '1';
-    scrollThumb.style.opacity = '1';
+    // scrollThumb.style.opacity = '1';
     if(delay)
         clearTimeout(delay);
     delay = setTimeout(function(){
         scrollTrack.style.opacity = '0.2';
-        scrollThumb.style.opacity = '0.2';
+        // scrollThumb.style.opacity = '1';
     }, 1000);
 });
 
@@ -78,42 +78,54 @@ if(window.scrollY > 600) {
 }
 
 //make Modal
-let makeModal = function(innerHtml){
+/**
+ * 
+ * @param {string} titleText 
+ * @param {string} innerHtml 
+ */
+let makeModal = function(titleText, innerHtml){
     let modal = document.createElement('div');
     modal.classList.add('modal');
     let body = document.createElement('div');
     body.classList.add('body');
     body.innerHTML = innerHtml;
+    if(titleText || titleText.trim().length > 0){
+        let title = document.createElement('p');
+        title.classList.add('title');
+        title.innerHTML = titleText;
+        body.prepend(title);
+    }
     let closeBtn = document.createElement('a');
     closeBtn.classList.add('close');
-    closeBtn.addEventListener("click", function(){
+    closeBtn.addEventListener("click", function(e){
         modal.style.opacity = 0;
         setTimeout(function() {
             modal.remove();
+            document.querySelector('html').style.overflowY = 'auto';
         }, 300);
     })
-    body.prepend(closeBtn);
+    modal.addEventListener('click', function(e){
+        if(e.target.classList.contains('modal')){
+            modal.style.opacity = 0;
+            setTimeout(function() {
+                modal.remove();
+                document.querySelector('html').style.overflowY = 'auto';
+            }, 300);
+        } else {
+            return;
+        }
+    });
+    body.append(closeBtn);
     modal.appendChild(body);
-    return modal;
+    document.body.prepend(modal);
+    document.querySelector('html').style.overflowY = 'hidden';
 }
-
-//loading animation modal
-document.querySelector("#caution").addEventListener("click", function(){
-    document.body.prepend(makeModal(
-        `<p class="title">주의사항</p>
-        <p>이 페이지는 인터넷 회선이 빠른 경우 제대로 테스트 하기 어려울 수 있으니,
-            개발자 옵션에서 네트워크 속도를 강제로 느리게 한 후 보시길 권장합니다.</p>
-            <br>
-        <strong>개발자 도구 ‣ 네트워크 ‣ 우측 상단의 No throtting ‣ Slow 3G</strong>
-        <br><p>테스트 후에는 꼭 <strong>"No throttling"</strong> 으로 바꿔주세요.</p>`));
-});
 
 //scroll animation
 const contentObserver = new IntersectionObserver((e) => {
     e.forEach(element => {
         //on view
         if(element.isIntersecting){
-            console.log(element)
             element.target.style.opacity = '1';
             element.target.style.transform = 'translateY(0)';
         } else {
@@ -127,7 +139,6 @@ const headingObserver = new IntersectionObserver((e) => {
     e.forEach(element => {
         //on view
         if(element.isIntersecting){
-            console.log(99)
             element.target.style.opacity = '1';
             element.target.style.transform = 'translateX(0)';
         } else {
@@ -143,7 +154,7 @@ let checkUserAgent = function(){
     if (navigator.userAgent.match(/Android/i) ||
         navigator.userAgent.match(/iPhone/i)) {
         alert(`본 포트폴리오 페이지는 데스크탑 환경(1440px 이상)에 최적화 되어있습니다.
-        권장 해상도 : 1920x1080`);
+권장 해상도 : 1920x1080`);
     }
 }
 //check window size 
@@ -151,5 +162,79 @@ let checkWindowSize = function(){
     let isDesktop = window.matchMedia('(min-width: 1439px)').matches;
     if(!isDesktop)
         alert(`본 포트폴리오 페이지는 데스크탑 환경(1440px 이상)에 최적화 되어있습니다.
-        권장 해상도 : 1920x1080`)
+권장 해상도 : 1920x1080`)
+}
+
+
+
+
+// Modal Functions
+//loading animation modal
+let loadingAnimationModal = function(){
+    makeModal("주의사항",
+        `<p>이 페이지는 인터넷 회선이 빠른 경우 제대로 테스트 하기 어려울 수 있으니,
+            개발자 옵션에서 네트워크 속도를 강제로 느리게 한 후 보시길 권장합니다.</p>
+            <br>
+        <strong>개발자 도구 ‣ 네트워크 ‣ 우측 상단의 No throtting ‣ Slow 3G</strong>
+        <br><p>테스트 후에는 꼭 <strong>"No throttling"</strong> 으로 바꿔주세요.</p>`);
+};
+let bugsStyleGuide = function(){
+    makeModal("Bugs Redesign Style Guide",
+        `<img src="./images/bugs-redesign/bugs-style-guide.png" alt="bugs-style-guide">`
+    )
+}
+let bugsWireframe = function(){
+    makeModal("Bugs Redesign Wireframe",
+    `<div style="display: flex; width: calc(85vw - 100px);gap: 20px;">
+    <div>
+    <p class="content-lg">Desktop</p>
+    <img src="./images/bugs-redesign/bugs-redesign-wireframe-desktop.jpg">
+    </div>
+    <div>
+    <p class="content-lg">Tablet</p>
+    <img src="./images/bugs-redesign/bugs-redesign-wireframe-tablet.png">
+    </div>
+    <div>
+    <p class="content-lg">Mobile</p>
+    <img src="./images/bugs-redesign/bugs-redesign-wireframe-mobile.png">
+    </div>
+    </div>`)
+}
+let selpixStyleGuide = function(){
+    makeModal("Selpix Style Guide",
+    `<img src="./images/selpix/Seplix Style Guide.png">`);
+}
+let selpixWireframe = function(){
+    makeModal("Selpix Wireframe",
+    `<div style="display: flex; width: calc(85vw - 100px);gap: 20px;">
+    <div>
+    <p class="content-lg">Desktop</p>
+    <img src="./images/selpix/selpix-wireframe-desktop.png">
+    </div>
+    <div>
+    <p class="content-lg">Tablet</p>
+    <img src="./images/selpix/selpix-wireframe-tablet.png">
+    </div>
+    <div>
+    <p class="content-lg">Mobile</p>
+    <img src="./images/selpix/selpix-wireframe-mobile.png">
+    </div>
+    </div>`)
+};
+let graphicDesignImages = document.querySelectorAll('.swiper .swiper-slide');
+console.log(graphicDesignImages)
+graphicDesignImages.forEach(function(element) {
+    let image = element.querySelector("img");
+    image.addEventListener("click", function(e){
+        makeModal("이미지 상세보기", `<div style="text-align: center;"><img src="${image.src}"></div>`);
+    })
+});
+let clockModal = function(){
+    makeModal("Clock", 
+    `<iframe src="./project/clock/index.html" frameborder="0"></iframe>`
+    )
+}
+let buyListModal = function(){
+    makeModal("구매목록 생성기",
+    `<iframe src="./project/buy-list/index.html" frameborder="0" style="height:70vh"></iframe>`)
 }
